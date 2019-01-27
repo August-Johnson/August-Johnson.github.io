79c8bd233989048f5d9d7637ef0b5df7653f7873
August Johnson <gusjohnson98@gmail.com>
2019-01-26 23:11:09 -0600

    Update app.js

--- week7/train-schedule/assets/javascript/app.js
+++ week7/train-schedule/assets/javascript/app.js
@@ -17,10 +17,20 @@ var destination = "";
 var startTime = "";
 var frequency = "";
 
-// calculated data, not stored in firebase
-var nextArrival = "";
-var minutesAway = "";
+// trains array variable
+var trains = [];
 
+// empty object to store the new train data inside
+var newTrain = {
+    name: "",
+    destination: "",
+    startTime: "",
+    frequency: ""
+};
+
+// calculated data, not stored in firebase
+var nextArrival = [];
+var minutesAway = [];
 
 // submit button on-click event
 $('#submit-train').on('click', function (event) {
@@ -29,45 +39,58 @@ $('#submit-train').on('click', function (event) {
     name = $('#train-name').val().trim();
     destination = $('#train-destination').val().trim();
     startTime = $('#train-time').val().trim();
-    frequency = $('#train-frequency').val().trim();
-    // ------------------------------------------------------------ 
-
-    // getting the start time into the right format
     startTime = moment(startTime, "HH:mm A").format("hh:mm A");
-    console.log(startTime);
+    frequency = $('#train-frequency').val().trim();
+
+    // checking if any of the inputs fields are empty
+    if (name !== "" && destination !== "" && startTime !== "" && frequency !== "") {
+
+        newTrain.name = name;
+        newTrain.destination = destination;
+        newTrain.startTime = startTime;
+        newTrain.frequency = frequency;
+
+        trains.push(newTrain);
+
+
+        // storing the trains array value to firebase
+        database.ref().set({
+            trains: trains,
+        });
+
+        $('#train-name').val('');
+        $('#train-destination').val('');
+        $('#train-time').val('');
+        $('#train-frequency').val('');
+
+    }
+    // if any input fields are empty, alert the user they haven't filled everything out
+    else {
+        console.log("Please finish filling out the form!");
+    }
 
     // calculating next arrival and formatting it
-    nextArrival = moment(startTime, "hh:mm A").add(frequency, "m");
-    nextArrival = moment(nextArrival).format("hh:mm A");
-    console.log(nextArrival);
+    // nextArrival.push(moment(startTime, "hh:mm A").add(frequency, "m"));
+    // nextArrival = moment(nextArrival).format("hh:mm A");
+    // console.log(nextArrival);
+
 
     // getting the minutes away
 
-
-
-
-    database.ref().push({
-        name: name,
-        destination: destination,
-        startTime: startTime,
-        frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
-
     // resets the input fields to empty
-    $('#train-name').val('');
-    $('#train-destination').val('');
-    $('#train-time').val('');
-    $('#train-frequency').val('');
+
 
 });
 // adding train to the table / list
-database.ref().on('child_added', function (childSnapshot) {
-    $('tbody').append('<tr>'
-        + '<td>' + name + '</td>'
-        + '<td>' + destination + '</td>'
-        + '<td>' + frequency + '</td>'
-        + '<td>' + nextArrival + '</td>'
-        + '<td>' + minutesAway + '</td>'
-        + '</tr>');
+// change back to .on('child_added'), later also childSnapshot
+
+database.ref().on("value", function (snapshot) {
+    trains = snapshot.val().trains;
+    //     $('tbody').html('<tr>'
+    //         + '<td>' + snapshot.val().name + '</td>'
+    //         + '<td>' + snapshot.val().destination + '</td>'
+    //         + '<td>' + snapshot.val().frequency + '</td>'
+    //         + '<td>' + nextArrival + '</td>'
+    //         + '<td>' + minutesAway + '</td>'
+    //         + '</tr>');
 });
