efa49ff3d877d3bf7ebb19883303a7db9ea3c6ae
August Johnson <gusjohnson98@gmail.com>
2019-02-17 17:27:51 -0600

    spotify and movie function

--- week10/liri.js
+++ week10/liri.js
@@ -1,49 +1,122 @@
+// require packages
 require("dotenv").config();
-var keys = require("./keys.js");
-// var spotify = keys.spotify;
-
 
 var axios = require("axios");
 var moment = require("moment");
 var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-
-// action type and secondary input
+// action type and secondary input (s0ng, movie, band etc.)
 var userCommand = process.argv[2];
 var secondCommand = process.argv.splice(3).join(" ");
 
 
-// function for each command type (use ajax api calls)
+// spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
 
+
+// function for each command type (use ajax api calls)
 // concert-this function
 function getConcert() {
     var artist = secondCommand;
 
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
         for (i = 0; i < response.data.length; i++) {
-            console.log("VENUE NAME: " + response.data[i].venue.name + 
-            "\n VENUE LOCATION: " + response.data[i].venue.city + " " + response.data[i].venue.region + 
-            "\n DATE OF THE VENUE: " + moment(response.data[i].datetime).format("MM/DD/YYYY") + "\n-----------------------------------");
+            // if the location doesn't have a region (areas in Europe), then don't print the empty region with an extra comma
+            if (response.data[i].venue.region === "") {
+                console.log(i + ':\nVENUE NAME: "' + response.data[i].venue.name +
+                    '"\nVENUE LOCATION: "' + response.data[i].venue.city + ", " + response.data[i].venue.country +
+                    '"\nDATE OF THE VENUE: ' + moment(response.data[i].datetime).format("MM/DD/YYYY") + '\n-----------------------------------');
+            }
+            else {
+                console.log(i + '\nVENUE NAME: "' + response.data[i].venue.name +
+                    '"\nVENUE LOCATION: "' + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country +
+                    '"\nDATE OF THE VENUE: ' + moment(response.data[i].datetime).format("MM/DD/YYYY") + '\n-----------------------------------');
+            }
         }
     });
 }
+
 // spotify-this-song function
 function getSong() {
     var songName = secondCommand;
-
-    spotify.search({ type: 'track', query: songName, limit: 3 }, function (err, response) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        for (i = 0; i < response.tracks.items.length; i++) {
-            console.log("\nArtist(s) name: " + response.tracks.items[i].album.artists[0].name + 
-            "\nSong name: " + response.tracks.items[i].name + "\n--------------------------------------");
-        }
-    });
+    // if the user didn't enter a song name, print the default of 'The Sign, by Ace of Base'
+    if (songName === "" || songName === undefined || songName === null) {
+        var defaultSong = 'The Sign';
+        spotify.search({ type: 'track', query: defaultSong, limit: 10 }, function (err, response) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log('--------------------------------------\n\nARTIST NAME: "' + response.tracks.items[9].album.artists[0].name +
+                '"\nSONG NAME: "' + response.tracks.items[9].name +
+                '"\nALBUM NAME: "' + response.tracks.items[9].album.name +
+                '"\nLINK TO SONG: "' + response.tracks.items[9].preview_url + '"\n');
+        });
+    }
+    else {
+        // if the user did enter a song name
+        spotify.search({ type: 'track', query: songName, limit: 5 }, function (err, response) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            for (i = 0; i < response.tracks.items.length; i++) {
+                // if there is no preview, just print the link to the full song
+                if (response.tracks.items[i].preview_url === null) {
+                    console.log('--------------------------------------\n\nARTIST NAME: "' + response.tracks.items[i].album.artists[0].name +
+                        '"\nSONG NAME: "' + response.tracks.items[i].name +
+                        '"\nALBUM NAME: "' + response.tracks.items[0].album.name +
+                        '"\nLINK TO SONG: "' + response.tracks.items[i].album.external_urls.spotify + '"\n');
+                }
+                else {
+                    console.log('--------------------------------------\n\nARTIST NAME: "' + response.tracks.items[i].album.artists[0].name +
+                        '"\nSONG NAME: "' + response.tracks.items[i].name +
+                        '"\nALBUM NAME: "' + response.tracks.items[0].album.name +
+                        '"\nLINK TO SONG: "' + response.tracks.items[i].preview_url + '"\n');
+                }
+            }
+        });
+    }
 }
-
 // movie-this function
+function getMovie() {
+    var movieTitle = secondCommand;
+    // if the user didn't put in a movie title, print the default data of 'Mr. Nobody'
+    if (movieTitle === "" || movieTitle === undefined || movieTitle === null) {
+        var defaultMovie = 'Mr. Nobody';
+
+        axios.get("http://www.omdbapi.com/?t=" + defaultMovie + "&plot=full&apikey=3d42b8e8").then(function (response) {
+            console.log('--------------------------------------\n\nMOVIE TITLE: "' + response.data.Title +
+                '"\nRELEASE YEAR: ' + response.data.Year +
+                '\nIMDB RATING: ' + response.data.imdbRating +
+                '\nROTTEN TOMATOES RATING: ' + response.data.Ratings[1].Value +
+                '\nCOUNTRY: "' + response.data.Country +
+                '"\nLANGUAGE: "' + response.data.Language +
+                '"\nACTORS: "' + response.data.Actors +
+                '"\n\nPLOT: "' + response.data.Plot +
+                '"\n--------------------------------------\n(If you haven\'t watched Mr. Nobody, you should! It\'s on Netflix!)\nhttp://www.imdb.com/title/tt0485947/');
+        })
+    }
+    // if the user did put in a movie title
+    else {
+        axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&plot=full&apikey=3d42b8e8").then(function (response) {
+            console.log('--------------------------------------\n\nMOVIE TITLE: "' + response.data.Title +
+                '"\nRELEASE YEAR: ' + response.data.Year +
+                '\nIMDB RATING: ' + response.data.imdbRating +
+                '\nROTTEN TOMATOES RATING: ' + response.data.Ratings[1].Value +
+                '\nCOUNTRY: "' + response.data.Country +
+                '"\nLANGUAGE: "' + response.data.Language +
+                '"\nACTORS: "' + response.data.Actors +
+                '"\n\nPLOT: "' + response.data.Plot + '"\n');
+        });
+    }
+}
 // do-what-it-says function
 
 // switch statement based on users command type
@@ -57,7 +130,7 @@ switch (userCommand) {
         break;
 
     case "movie-this":
-        console.log("you chose the movie command");
+        getMovie();
         break;
 
     case "do-what-it-says":
