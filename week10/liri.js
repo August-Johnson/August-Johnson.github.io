d3948472f4960fe26c5d1361920e9aa52adf7444
August Johnson <gusjohnson98@gmail.com>
2019-02-18 03:02:43 -0600

    final commands and bonus

--- week10/liri.js
+++ week10/liri.js
@@ -1,53 +1,76 @@
-// require packages
+// Require packages calls.
 require("dotenv").config();
-
+var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+// Putting process.argv[3] into a better format.
+process.argv[3] = process.argv.splice(3).join(" ");
+console.log(process.argv[3]);
+// Function for switch statement based on the users' command type.
+function getUserCommand(commandType, commandValue) {
+    switch (commandType) {
+        case "concert-this":
+            getConcert(commandValue);
+            break;
 
-// action type and secondary input (s0ng, movie, band etc.)
-var userCommand = process.argv[2];
-var secondCommand = process.argv.splice(3).join(" ");
+        case "spotify-this-song":
+            getSong(commandValue);
+            break;
 
+        case "movie-this":
+            getMovie(commandValue);
+            break;
 
-// spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
+        case "do-what-it-says":
+            getFileText(commandType, commandValue);
+            break;
 
-
-// function for each command type (use ajax api calls)
-// concert-this function
-function getConcert() {
-    var artist = secondCommand;
+        default:
+            console.log('\nINVALID COMMAND, PLEASE USE ONE OF THE FOLLOWING VALID INPUTS: \n\n' +
+                '1)"concert-this" + "artist name"\n\n' +
+                '2)"spotify-this-song" + "song name"\n\n' +
+                '3)"movie-this" + "movie name"\n\n' +
+                '4)"do-what-it-says"');
+    }
+}
+// Appending each command to log.txt
+    var textToAppend = process.argv[2] + ' ' + process.argv[3] + '\n';
+    fs.appendFile("log.txt", textToAppend, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+// Functions for each command type.
+// Function for the 'concert-this' command.
+function getConcert(artist) {
 
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
-        for (i = 0; i < response.data.length; i++) {
-            // if the location doesn't have a region (areas in Europe), then don't print the empty region with an extra comma
-            if (response.data[i].venue.region === "") {
-                console.log(i + ':\nVENUE NAME: "' + response.data[i].venue.name +
-                    '"\nVENUE LOCATION: "' + response.data[i].venue.city + ", " + response.data[i].venue.country +
-                    '"\nDATE OF THE VENUE: ' + moment(response.data[i].datetime).format("MM/DD/YYYY") + '\n-----------------------------------');
+        // If there are no concerts found, prints a message to the user telling them so.
+        if (response.data.length === 0) {
+            console.log('\nZERO RESULTS FOUND.\n');
+        }
+        // Otherwise, run normally. I made 'variable i' start at 1 for the sake of displaying the number of each response.
+        for (i = 1; i < response.data.length + 1; i++) {
+            // If the location doesn't have a region (areas in Europe), then don't print the empty region with an extra comma and white space.
+            if (response.data[i - 1].venue.region === "") {
+                console.log('\n' + i + ':\nVENUE NAME: "' + response.data[i - 1].venue.name +
+                    '"\nVENUE LOCATION: "' + response.data[i - 1].venue.city + ", " + response.data[i - 1].venue.country +
+                    '"\nDATE OF THE VENUE: ' + moment(response.data[i - 1].datetime).format("MM/DD/YYYY") + '\n\n-----------------------------------');
             }
             else {
-                console.log(i + '\nVENUE NAME: "' + response.data[i].venue.name +
-                    '"\nVENUE LOCATION: "' + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country +
-                    '"\nDATE OF THE VENUE: ' + moment(response.data[i].datetime).format("MM/DD/YYYY") + '\n-----------------------------------');
+                console.log('\n' + i + '\nVENUE NAME: "' + response.data[i - 1].venue.name +
+                    '"\nVENUE LOCATION: "' + response.data[i - 1].venue.city + ", " + response.data[i - 1].venue.region + ", " + response.data[i - 1].venue.country +
+                    '"\nDATE OF THE VENUE: ' + moment(response.data[i - 1].datetime).format("MM/DD/YYYY") + '\n\n-----------------------------------');
             }
         }
     });
 }
-
-// spotify-this-song function
-function getSong() {
-    var songName = secondCommand;
-    // if the user didn't enter a song name, print the default of 'The Sign, by Ace of Base'
+// Function for the 'spotify-this-song' command.
+function getSong(songName) {
+    // If the user didn't enter a song name, print the data of the default-('The Sign, by Ace of Base').
     if (songName === "" || songName === undefined || songName === null) {
         var defaultSong = 'The Sign';
         spotify.search({ type: 'track', query: defaultSong, limit: 10 }, function (err, response) {
@@ -61,13 +84,13 @@ function getSong() {
         });
     }
     else {
-        // if the user did enter a song name
+        // If the user did enter a song name, run normally.
         spotify.search({ type: 'track', query: songName, limit: 5 }, function (err, response) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
             for (i = 0; i < response.tracks.items.length; i++) {
-                // if there is no preview, just print the link to the full song
+                // If there is no preview, just print the link to the full song instead.
                 if (response.tracks.items[i].preview_url === null) {
                     console.log('--------------------------------------\n\nARTIST NAME: "' + response.tracks.items[i].album.artists[0].name +
                         '"\nSONG NAME: "' + response.tracks.items[i].name +
@@ -84,10 +107,9 @@ function getSong() {
         });
     }
 }
-// movie-this function
-function getMovie() {
-    var movieTitle = secondCommand;
-    // if the user didn't put in a movie title, print the default data of 'Mr. Nobody'
+// Function for the 'movie-this' command.
+function getMovie(movieTitle) {
+    // If the user didn't put in a movie title, print the default data of 'Mr. Nobody'.
     if (movieTitle === "" || movieTitle === undefined || movieTitle === null) {
         var defaultMovie = 'Mr. Nobody';
 
@@ -100,10 +122,10 @@ function getMovie() {
                 '"\nLANGUAGE: "' + response.data.Language +
                 '"\nACTORS: "' + response.data.Actors +
                 '"\n\nPLOT: "' + response.data.Plot +
-                '"\n--------------------------------------\n(If you haven\'t watched Mr. Nobody, you should! It\'s on Netflix!)\nhttp://www.imdb.com/title/tt0485947/');
+                '"\n--------------------------------------\n(If you haven\'t watched Mr. Nobody, you should! It\'s on Netflix!)\n"http://www.imdb.com/title/tt0485947/"');
         })
     }
-    // if the user did put in a movie title
+    // If the user did put in a movie title, run normally.
     else {
         axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&plot=full&apikey=3d42b8e8").then(function (response) {
             console.log('--------------------------------------\n\nMOVIE TITLE: "' + response.data.Title +
@@ -117,30 +139,27 @@ function getMovie() {
         });
     }
 }
-// do-what-it-says function
-
-// switch statement based on users command type
-switch (userCommand) {
-    case "concert-this":
-        getConcert();
-        break;
-
-    case "spotify-this-song":
-        getSong();
-        break;
-
-    case "movie-this":
-        getMovie();
-        break;
-
-    case "do-what-it-says":
-        console.log("you chose the other one");
-        break;
-
-    // default:
-    //     console.log("INVALID COMMAND, PLEASE USE ONE OF THE FOLLOWING VALID INPUTS: \n" +
-    //         "1)'concert-this' + 'artist name' \n" +
-    //         "2)'spotify-this-song' + 'song name' \n" +
-    //         "3)'movie-this' + 'movie name' \n" +
-    //         "4)'do-what-it-says'");
+// function for the 'do-what-it-says' command
+function getFileText(fileCommand, fileValue) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        // Formatting the text file into an array and then seperating each part into a seperate command value. 
+        // The first one being the command and the second one being the parameter or arguement for the command.
+        var dataArr = data.split(",");
+        fileCommand = dataArr[0];
+        fileValue = dataArr[1];
+        // Taking the command value and putting it into an array so I can remove the double quotes.
+        // The concert-this command didn't run into any issues when I manually entered the second arguement with double quotes,
+        // but it freaked out when the readFile tried to put in the double quotes.
+        fileValue = fileValue.split("");
+        fileValue.splice(fileValue.indexOf('"'), 1);
+        fileValue.splice(fileValue.lastIndexOf('"'), 1);
+        fileValue = fileValue.join('');
+        // Running the getUserCommand function and passing in the random.txt values.
+        getUserCommand(fileCommand, fileValue);
+    });
 }
+// Run the function to check the users' command upon start.
+getUserCommand(process.argv[2], process.argv.splice(3).join(" "));
